6dd26e15be974e265526878c19d94ae2cd1d26ac
Maycon pequeno <pequenosilvamaycon@gmail.com>
2026-02-25 08:50:19 -0300

    Update atividade1-12-18.py

--- atividade1-12-18.py
+++ atividade1-12-18.py
@@ -1,34 +1,38 @@
+def calculo_ponto(a,b):
+    D = abs(a - b)
+    if D < 3:
+        return 100.0
+    elif  (D >= 3) and (D <= 5):
+        return 80.0
+    else:
+        return  80.0 - (D - 5) / 5
 def main():
     maior_pontuacao = -1.0
     inscricao_vencedor = 0
-    tempo_padrao = []
-    for i in range(1, 4):
-        tempo = float(input(f"Digite o tempo-padrão da etapa {i}: "))
-        tempo_padrao.append(tempo)
+    tempo1 = float(input(f"Digite o tempo-padrão da etapa : "))
+    tempo2 = float(input(f"Digite o tempo-padrão da etapa : "))
+    tempo3 = float(input(f"Digite o tempo-padrão da etapa : "))
 
     inscricao = int(input("Número de inscrição (9999 para encerrar): "))
     while inscricao != 9999:
-
-        pontos_equipe = []
-        for i in range(3):
-            t_equipe = float(input(f"Digite o tempo da equipe na etapa {i+1}: "))
-            D = tempo_padrao[i] - t_equipe
-            if D < 3:
-                p = 100.0
-            elif D <= 5:
-                p = 80.0
-            else:
-                p = 80.0 - (D - 5) / 5
-            pontos_equipe.append(p)
-        total_pontos = sum(pontos_equipe)
+        t_e1 = float(input(f"Digite o tempo da equipe na etapa : "))
+        t_e2 = float(input(f"Digite o tempo da equipe na etapa : "))
+        t_e3 = float(input(f"Digite o tempo da equipe na etapa : "))
+        ponto1= calculo_ponto(tempo1,t_e1)
+        ponto2=calculo_ponto(tempo1,t_e1)
+        ponto3=calculo_ponto(tempo1,t_e1)
+        total_pontos = ponto1 + ponto2 + ponto3
+        
         print(f"Equipe: {inscricao}")
-        print(f"Pontos Etapa 1: {pontos_equipe[0]:.2f} | Etapa 2: {pontos_equipe[1]:.2f} | Etapa 3: {pontos_equipe[2]:.2f}")
+        print(f"Pontos Etapa 1: {t_e1:.2f} | Etapa 2: {t_e2:.2f} | Etapa 3: {t_e3:.2f}")
         print(f"Total: {total_pontos:.2f}")
         print("-" * 40)
+       
+        if total_pontos > maior_pontuacao:
+            maior_pontuacao = total_pontos
+            inscricao_vencedor = inscricao
         inscricao = int(input("Número de inscrição (9999 para encerrar): "))
-    if total_pontos > maior_pontuacao:
-        maior_pontuacao = total_pontos
-        inscricao_vencedor = inscricao
+
     if maior_pontuacao != -1:
         print("\n" + "="*40)
         print(f"A EQUIPE VENCEDORA FOI A DE INSCRIÇÃO: {inscricao_vencedor}")
