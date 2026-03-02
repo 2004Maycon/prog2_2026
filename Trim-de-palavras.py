2e16ce9355aa04c8420f59bdf499f0fb40edfbcc
2004Maycon <pequenosilvamaycon@gmail.com>
2026-03-02 17:01:51 -0300

    Update Trim-de-palavras.py

--- Trim-de-palavras.py
+++ Trim-de-palavras.py
@@ -1,9 +1,13 @@
 def main():
     texto = str(input("qual o texto: "))
     texto_revisado = str()
+    nada = str()
     for letra in texto:
-        
-        texto_revisado+= letra
+        if letra in "," or letra in "." or  letra in "/" or  letra in "()" or  letra in "-" or  letra in "_" or  letra in "=" or  letra in "!" or  letra in ":" or  letra in ";" or  letra in "?":
+            nada+= letra
+        else:
+            texto_revisado+= letra
     print(f"texto anterior {texto}")
     print(f"texto revisado {texto_revisado}")
+    print(nada)
 main()
