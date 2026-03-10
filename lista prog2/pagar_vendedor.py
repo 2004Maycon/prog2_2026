6787d6b6c0dfd7fc42a53fdaed4eb5cc38ab1968
Maycon pequeno <pequenosilvamaycon@gmail.com>
2026-03-10 09:00:04 -0300

    Update pagar_vendedor.py

--- lista prog2/pagar_vendedor.py
+++ lista prog2/pagar_vendedor.py
@@ -1,9 +1,10 @@
 def main():
     
     quantidade_recebeu_comissao=[]
-    #valor_recebido =input()
+    valor_recebido =200
     valor_venda = float(input("qual valor de venda: "))
     for i in range(1):
-        valor_venda*(9/100)
-    print(valor_venda)
+        comisao = (valor_venda*(9/100))+valor_recebido
+        quantidade_recebeu_comissao.append(comisao)
+    print(quantidade_recebeu_comissao)
 main()
