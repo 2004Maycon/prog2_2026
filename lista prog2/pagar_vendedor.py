a233e285d214831b6083795de3e0262fa6a60254
Maycon pequeno <pequenosilvamaycon@gmail.com>
2026-03-10 08:15:07 -0300

    Create pagar_vendedor.py

---
+++ lista prog2/pagar_vendedor.py
@@ -0,0 +1,9 @@
+def main():
+    
+    quantidade_recebeu_comissao=[]
+    #valor_recebido =input()
+    valor_venda = float(input("qual valor de venda: "))
+    for i in range(1):
+        valor_venda*(9/100)
+    print(valor_venda)
+main()
