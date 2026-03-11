ab3b198d6f64db2ebdfba3d8a9734f7ca6a8be3d
2004Maycon <pequenosilvamaycon@gmail.com>
2026-03-11 16:12:46 -0300

    Create intencesao.py

---
+++ lista prog2/intencesao.py
@@ -0,0 +1,10 @@
+def main():
+    lista_A = [1,5,7,9,10,32]
+    lista_B = [10,2,5,7,9,22]
+    intercesao =[]
+    aux = int()
+    for  lista_B in lista_A :
+        intercesao.append(lista_A)
+    print(F"essa é a lista A {lista_A}, essa é a lista B {lista_B}")
+    print(f"essa é a intercessão {intercesao}")
+main() 
