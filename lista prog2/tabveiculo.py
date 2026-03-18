3f554430d0b1d20ca876c5d332c77a1d09b62574
Maycon pequeno <pequenosilvamaycon@gmail.com>
2026-03-18 08:19:01 -0300

    Delete tabveiculo.py

--- lista prog2/tabveiculo.py
+++
@@ -1,8 +0,0 @@
-def main():
-    arq= open("tabveiculos.txt","rt",encoding="utf8")
-    linha = arq.readline()
-    while linha != "":
-        print(linha,end=" ")
-        linha =  arq.readline()
-    arq.close()
-main()
