387ee7533cc9cf28943b29aedc26b33d1400f7a1
Maycon pequeno <pequenosilvamaycon@gmail.com>
2026-03-24 20:46:07 -0300

    Update Trim-de-palavras.py

--- Trim-de-palavras.py
+++ Trim-de-palavras.py
@@ -2,8 +2,8 @@ def main():
     texto = str(input("qual o texto: "))
     texto_revisado = str()
     for letra in texto:
-        
-        texto_revisado+= letra
+        while letra in "abcdefghijklmnopqrstuvxwyz123456789":
+            texto_revisado+= letra
     print(f"texto anterior {texto}")
     print(f"texto revisado {texto_revisado}")
 main()
